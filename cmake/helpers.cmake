e07020ac33ec307e40fcdc0ac67e9ba6ca35003d
Rafal Rudnicki <rafal.rudnicki@intel.com>
2023-11-16 16:02:30 +0100

    cmake: include FetchContent only when needed

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -9,7 +9,6 @@
 # CMake modules that check whether the C/C++ compiler supports a given flag
 include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
-include(FetchContent)
 
 # Sets ${ret} to version of program specified by ${name} in major.minor format
 function(get_program_version_major_minor name ret)
@@ -177,6 +176,8 @@ function(FetchContentCheckout_Declare SRC_DIR GIT_REPOSITORY GIT_BRANCH GIT_HASH
         WORKING_DIRECTORY ${external-content-dir})
     execute_process(COMMAND git reset --hard ${GIT_HASH}
         WORKING_DIRECTORY ${external-content-dir})
+        
+    include(FetchContent)
     FetchContent_Declare(
         ${name}
         GIT_REPOSITORY ${GIT_REPOSITORY}
